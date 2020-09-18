9ffe115da6090bea3c1bad249ed8ad9579457f0a
lixueqingsd <xqli@sdu.edu.cn>
2020-09-18 16:58:42 +0800

    lxq

--- pages/medicare/physicalexaminationReportView.vue
+++ pages/medicare/physicalexaminationReportView.vue
@@ -4,7 +4,7 @@
 		<view class="adBaseView">
 			<view class="adRowView">
 				<view class="headView">工号：</view>
-				<view class="input-text">200799013517</view>
+				<view class="input-text">{{perNum}}</view>
 			</view>
 			<view class="bottomLine" />
 		</view>
@@ -12,7 +12,7 @@
 		<view class="adBaseView">
 			<view class="adRowView">
 				<view class="headView">姓名：</view>
-				<view class="input-text">李学庆</view>
+				<view class="input-text">{{perName}}</view>
 			</view>
 			<view class="bottomLine" />
 		</view>
@@ -20,25 +20,25 @@
 		<view class="adBaseView">
 			<view class="adRowView">
 				<view class="headView">所在单位：</view>
-				<view class="input-text">软件学院</view>
+				<view class="input-text">{{collegeName}}</view>
 			</view>
 			<view class="bottomLine" />
 		</view>
 		
 	<uni-section title="历年体检报告" type="line" />
-	 <view class="item-t" v-for="(item, index) in orderList" :key="index">
+	 <view class="item-t" v-for="(item, index) in dataList" :key="index">
 	 	<view style="display: flex;flex-direction: column;justify-content: right;align-items: flex-start;width: 100%;" >
 	 		<view style="margin-bottom: 3px;">
-	 			年度：<text style="font-weight: bold;" >2019</text>
+	 			年度：<text style="font-weight: bold;" >{{item.year}}</text>
 	 		</view>
 	 		<view style="margin-bottom: 3px;">
-	 			体检卷：<text style="font-weight: bold;" >B1</text>
+	 			体检卷：<text style="font-weight: bold;" >{{item.baseCheck}}</text>
 	 		</view>
 	 		<view style="margin-bottom: 3px;">
-	 			查体单位：<text style="font-weight: bold;" >山东大学齐鲁软件学院</text>
+	 			查体单位：<text style="font-weight: bold;" >{{item.checkUnit}}</text>
 	 		</view>
 	 		<view style="margin-bottom: 3px;">
-	 			基本套餐：<text style="font-weight: bold;" >套餐4</text>
+	 			基本套餐：<text style="font-weight: bold;" >{{item.projectName}}</text>
 	 		</view>	
 			<view style="margin-bottom: 3px;display: flex;flex-direction: row;">
 					<view style="color: blue;" >查体报告下载</view>
@@ -57,18 +57,26 @@
 </template>
 
 <script>
+	import {
+		physicalexaminationReportView
+	} from '@/api/medicare.js'
 	export default {
 	
 		data() {
 			return {
-				orderList:[1,2,3],
-	
+				perNum:'',
+				perName:'',
+				collegeName:'',
+				dataList:[],
 			};
 		},
 	
 		onShow() {
-			viewOrder().then(res => {
-				this.orderList = res.data.payList
+			physicalexaminationReportView().then(res => {
+				this.perNum = res.data.perNum
+				this.perName = res.data.perName
+				this.collegeName = res.data.collegeName
+				this.dataList = res.data.dataList
 			});
 		},
 		methods: {
