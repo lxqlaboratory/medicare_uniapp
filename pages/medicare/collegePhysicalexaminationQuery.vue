84355bd79e127ad1b3a00051217ee172a9b3672a
lixueqingsd <xqli@sdu.edu.cn>
2020-10-04 17:05:25 +0800

    lxq

--- pages/medicare/collegePhysicalexaminationQuery.vue
+++ pages/medicare/collegePhysicalexaminationQuery.vue
@@ -1,142 +1,140 @@
-</template>
-			<view class="adBaseView">
-				<view class="adRowView">
-					<view class="headView">
-						<view class="mustView">*</view>缴费方式
-					</view>
-					<view v-if="form.isFree===true" class="input-text">form.modelPayName</view>
-					<view v-else class="uni-list">
-						<view class="uni-list-cell">
-							<view class="uni-list-cell-db">
-								<picker @change="bindPickerModelPayChange" :value="form.modelPay" :range="payModels">
-									<view class="uni-input">{{payModels[form.modelPay]}}</view>
-								</picker>
-							</view>
-						</view>
-					</view>
-				</view>
-				<view class="bottomLine" />
+<template>
+	<view>
+		<view v-for="(item,index) in statisticsList" :key="index">
+			<view class="row">
+				<text class="row-title">查体单位: </text>
+				<text>{{item.checkUnitName}}</text>
 			</view>
-			<view  v-for="(item,index) in statisticList"  :key="index">
-					<view class="row">
-						<text class="row-title">缴费模式: </text>
-						<text>{{item.modelPayName}}</text>
-					</view>
-					<view class="row">
-						<text class="row-title">已缴费人数: </text>
-						<text>{{item.count0}}</text>
-					</view>
-					<view class="row">
-						<text class="row-title">未缴费人数: </text>
-						<text>{{item.count1}}</text>
-					</view>
-				<view class="bottomLine3"></view>
+			<view class="row">
+				<text class="row-title">申请人数: </text>
+				<text v-if="item.count==='0'">{{item.count}}</text>
+				<text v-else style="color: #e64340;" @click="doDetail(item.checkUnit)">{{item.count}}</text>
 			</view>
-			<view  v-for="(item,index) in studentList"  :key="index">
-					<view class="row">
-						<text class="row-title">学号姓名: </text>
-						<text>{{item.perNum}}</text>
-					</view>
-					<view class="row">
-						<text class="row-title">姓名: </text>
-						<text>{{item.perName}}</text>
-					</view>
-					<view class="row">
-						<text class="row-title">专业: </text>
-						<text>{{item.majorName}}</text>
-					</view>
-					<view class="row">
-						<text class="row-title">班级: </text>
-						<text>{{item.perClass}}</text>
-					</view>
-					<view class="row">
-						<text class="row-title">联系电话: </text>
-						<text>{{item.mobilePhone}}</text>
-					</view>
-				<view class="bottomLine3"></view>
-			</view>
+			<view class="bottomLine3"></view>
 		</view>
+		<view v-if="personList.length> 0">
+			<button class="button-cell2" @click="download()">导出人员名单</button>
+		</view>
+		<view v-for="(item,index) in personList" :key="index">
+			<view class="row">
+				<text class="row-title">工号: </text>
+				<text>{{item.perNum}}</text>
+				<text v-if="item.projectName===''" class="miniButton2" @click="doApply(item.perNum)">报名</text>
+				<text v-else class="miniButton2" @click="doApply(item.perNum)">修改</text>
+			</view>
+			<view class="row">
+				<text class="row-title">姓名: </text>
+				<text>{{item.perName}}</text>
+			</view>
+			<view class="row">
+				<text class="row-title">体检卷: </text>
+				<text>{{item.baseCheck}}</text>
+			</view>
+			<view class="row">
+				<text class="row-title">套餐: </text>
+				<text>{{item.projectName}}</text>
+			</view>
+			<view class="row">
+				<text class="row-title">联系电话: </text>
+				<text>{{item.mobilePhone}}</text>
+			</view>
+			<view class="row">
+				<button type='primary' class='s3bu' style="margin-top:15px;background-color:#7acfa6" @click="call(item.mobilePhone)">
+					<image src="/static/image/icon_clinic_tel_l.png"></image>
+					<view>联系电话</view>
+				</button>
+			</view>
+			<view class="bottomLine3"></view>
+		</view>
+	</view>
 </template>
 <script>
 	import {
-		collegeMedicareApplyQuery
+		collegePhysicalExaminationQuery
 	} from '@/api/medicare.js'
 	import {
-		collegeMedicareApplyStatisticsList
-	} from '@/api/login.js'
+		collegePhysicalExaminationInfoList
+	} from '@/api/medicare.js'
+
 	export default {
 		data() {
 			return {
-				perTypeCode:1,
-				grade:'',
-				gradeIndex:0,
-				gradeList:[],
-				year:'',
-				collegeNum:'',
-				modelPay:'0',
-				payStatus:'0',
-				studentList:[]
+				checkUnit:'00',
+				statisticsList: [],
+				personList: []
 			}
 		},
-		
+
 		methods: {
 			onLoad(option) {
-				this.perTypeCode = option.perTypeCode;
-				collegeMedicareApplyQuery({
-					perTypeCode:this.perTypeCode,
-				}).then(res => {
-					this.year = reds.data.year
-					this.collegeNum = res.data.collegeNum
-					this.grade = res.data.grade
-					this.gradeIndex = res.data.gradeIndex
-					this.gradeList = res.data.gradeList
+				collegePhysicalExaminationQuery().then(res => {
 					this.statisticsList = res.data.statisticsList
-				}).catch(err => {					
-				})		
+					this.personList = res.data.personList
+				}).catch(err => {})
 			},
-			
-			swtich(dataItemNum,payStatus){
-				console.log(dataItemNum)
+
+			doDetail(checkUnit) {
+				console.log(checkUnit)
+				this.checkUnit = checkUnit
+				collegePhysicalExaminationInfoList({
+					checkUnit: this.checkUnit,
+				}).then(res => {
+					this.personList = res.data
+				}).catch(err => {
+
+				})
+			},
+			doApply(perNum) {
 				uni.navigateTo({
-					url: '/pages/medicare/details?grade='+this.grade+'&dataItemNum='+dataItemNum+'&payStatus='+payStatus+'&mTypeCode='+this.mTypeCode
+					url: './physicalexaminationApply?perNum=' + perNum 
 				})
 			},
-				
-				
-			search(){
-				searchUndergraduate({
-					mTypeCode:this.mTypeCode,
-					grade:this.grade
-				}).then(res => {
-					this.list = res.data
-				}).catch(err => {
-					
-				})
+			call(mobilePhone) {
+				uni.makePhoneCall({
+					// 手机号
+					phoneNumber: mobilePhone,
+					// 成功回调
+					success: (res) => {
+						console.log('调用成功!')
+					},
+					// 失败回调
+					fail: (res) => {
+						console.log('调用失败!')
+						this.call_phone(); //重复调用一次
+					}
+				});
 			},
-			optionChange:function(e){
-				this.optiontIndex = e.target.value;
-				this.grade = this.options[this.optiontIndex];
-				console.log(this.grade);
-				showTotal({
-					mTypeCode:this.mTypeCode,
-					grade:this.grade
-				}).then(res => {
-					this.perPaid = res.data.perPaid
-					this.perUnpaid = res.data.perUnpaid
-					this.abanPaid = res.data.abanPaid
-					this.governPaid = res.data.governPaid
-					this.governUnpaid = res.data.governUnpaid
-					this.schPaid = res.data.schPaid
-					this.schUnpaid = res.data.schUnpaid
-					this.unApply = res.data.unApply
-					// console.log(this.stuList)
-					// this.current = this.stuList.length + 1
-				}).catch(err => {
-					
-				})
+			download() {
+				wx.downloadFile({
+					url: getApp().globalData.medicareurl + '/medicare/exportCollegePhysicalExaminationInfoList?checkUnit='+this.checkUnit,
+					header: {
+						"Content-Type": "application/json",
+						"Cookie": "JSESSIONID=" + getApp().globalData.vueSessionId
+					},
+					success: (res) => {
+						if (res.statusCode === 200) {
+							var filePath = res.tempFilePath;
+							wx.openDocument({
+								filePath: filePath,
+								success: function(res) {
+									console.log('打开文档成功')
+								},
+								fail: function(res) {
+									console.log(res);
+								},
+								complete: function(res) {
+									console.log(res);
+								}
+							})
+							uni.showToast({
+								title: `下载成功`,
+								icon: 'none'
+							})
+						}
+					}
+				});
 			}
-			
-		}
+		},
 	}
 </script>
 
